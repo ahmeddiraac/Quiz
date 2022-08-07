9079e8d61b96df3204aeeedef783d0f9f47c0b87
ahmeddiraac <axmedcali178@gmail.com>
2022-08-07 16:15:59 +0300

    error solved

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -11,8 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val btn_start:Button = findViewById(R.id.btn_start)
+
         val et_name:EditText = findViewById(R.id.et_name)
+
         btn_start.setOnClickListener {
             if (et_name.text.isEmpty()){
                 Toast.makeText(this, "Please Enter your Name", Toast.LENGTH_SHORT).show()
